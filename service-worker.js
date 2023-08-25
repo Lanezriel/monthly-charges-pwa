c17fb65716f0cfefea3e585a8f9a34710c2fda4d
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-25 14:48:37 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  a + "/_app/immutable/entry/app.1e2ae504.js",
+  a + "/_app/immutable/entry/app.166e51fa.js",
   a + "/_app/immutable/assets/0.fa9427ff.css",
-  a + "/_app/immutable/nodes/0.7e15adf1.js",
+  a + "/_app/immutable/nodes/0.ec74247e.js",
   a + "/_app/immutable/assets/svelte-logo.87df40b8.svg",
   a + "/_app/immutable/assets/github.1ea8d62e.svg",
   a + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
@@ -11,21 +11,21 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r
   a + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
-  a + "/_app/immutable/nodes/1.9c3e4837.js",
+  a + "/_app/immutable/nodes/1.6375a718.js",
   a + "/_app/immutable/assets/2.932fb878.css",
   a + "/_app/immutable/nodes/2.3493689c.js",
   a + "/_app/immutable/chunks/index.afc7ab19.js",
   a + "/_app/immutable/chunks/scheduler.b0c1c2c3.js",
-  a + "/_app/immutable/chunks/singletons.6036b5e2.js",
-  a + "/_app/immutable/chunks/stores.f39ac666.js",
-  a + "/_app/immutable/entry/start.fcab87c5.js"
+  a + "/_app/immutable/chunks/singletons.5ad4c815.js",
+  a + "/_app/immutable/chunks/stores.fec3988a.js",
+  a + "/_app/immutable/entry/start.f430b65e.js"
 ], f = [
   a + "/.nojekyll",
   a + "/favicon.png",
   a + "/icon192.png",
   a + "/icon512.png",
   a + "/robots.txt"
-], i = "1692967003083", c = self, l = `cache${i}`, m = r.concat(f), h = new Set(m);
+], i = "1692967710627", c = self, l = `cache${i}`, m = r.concat(f), h = new Set(m);
 c.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(l).then((s) => s.addAll(m)).then(() => {
