dcac045fefdfcbd127d036d86b7fcb9267140746
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-25 10:38:21 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>I'am a test page</h1>
+</section>
+
+<style>
+	h1 {
+		width: 100%;
+	}
+</style>
