c534404808eb9f39db98b18993c0e713b1f7537d
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-28 12:59:04 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  e + "/_app/immutable/entry/app.a4e38ac0.js",
+  e + "/_app/immutable/entry/app.c66048b3.js",
   e + "/_app/immutable/assets/0.cd0b29c6.css",
-  e + "/_app/immutable/nodes/0.e0dfb7e9.js",
+  e + "/_app/immutable/nodes/0.9149c860.js",
   e + "/_app/immutable/assets/svelte-logo.87df40b8.svg",
   e + "/_app/immutable/assets/github.1ea8d62e.svg",
   e + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
@@ -11,60 +11,55 @@ const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r
   e + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
   e + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
   e + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
-  e + "/_app/immutable/nodes/1.3d22f687.js",
+  e + "/_app/immutable/nodes/1.d922718d.js",
   e + "/_app/immutable/assets/2.5d969213.css",
   e + "/_app/immutable/nodes/2.ef4891fe.js",
   e + "/_app/immutable/nodes/3.fe160bd4.js",
   e + "/_app/immutable/chunks/index.afc7ab19.js",
   e + "/_app/immutable/chunks/scheduler.b0c1c2c3.js",
-  e + "/_app/immutable/chunks/singletons.6e7d0b35.js",
-  e + "/_app/immutable/chunks/stores.8b540965.js",
-  e + "/_app/immutable/entry/start.d4db84bf.js"
+  e + "/_app/immutable/chunks/singletons.b98020ae.js",
+  e + "/_app/immutable/chunks/stores.1114a870.js",
+  e + "/_app/immutable/entry/start.7882a39f.js"
 ], f = [
   e + "/.nojekyll",
   e + "/favicon.png",
   e + "/icon192.png",
   e + "/icon512.png",
   e + "/robots.txt"
-], l = "1693220182516", o = self, m = `cache${l}`, n = r.concat(f), h = new Set(n);
-console.log(n);
-o.addEventListener("install", (a) => {
+], i = "1693220339906", c = self, l = `cache${i}`, m = r.concat(f), h = new Set(m);
+c.addEventListener("install", (a) => {
   a.waitUntil(
-    caches.open(m).then((s) => s.addAll(
-      n.map(
-        (t) => self.location.hostname === "localhost" ? t : `/monthly-charges-pwa${t}`
-      )
-    )).then(() => {
-      o.skipWaiting();
+    caches.open(l).then((s) => s.addAll(m)).then(() => {
+      c.skipWaiting();
     })
   );
 });
-o.addEventListener("activate", (a) => {
+c.addEventListener("activate", (a) => {
   a.waitUntil(
     caches.keys().then(async (s) => {
       for (const t of s)
-        t !== m && await caches.delete(t);
-      o.clients.claim();
+        t !== l && await caches.delete(t);
+      c.clients.claim();
     })
   );
 });
 async function u(a) {
-  const s = await caches.open(`offline${l}`);
+  const s = await caches.open(`offline${i}`);
   try {
     const t = await fetch(a);
     return s.put(a, t.clone()), t;
   } catch (t) {
-    const c = await s.match(a);
-    if (c)
-      return c;
+    const n = await s.match(a);
+    if (n)
+      return n;
     throw t;
   }
 }
-o.addEventListener("fetch", (a) => {
+c.addEventListener("fetch", (a) => {
   if (a.request.method !== "GET" || a.request.headers.has("range"))
     return;
-  const s = new URL(a.request.url), t = s.protocol.startsWith("http"), c = s.hostname === self.location.hostname && s.port !== self.location.port, i = s.host === self.location.host && h.has(s.pathname), p = a.request.cache === "only-if-cached" && !i;
-  t && !c && !p && a.respondWith(
-    (async () => i && await caches.match(a.request) || u(a.request))()
+  const s = new URL(a.request.url), t = s.protocol.startsWith("http"), n = s.hostname === self.location.hostname && s.port !== self.location.port, o = s.host === self.location.host && h.has(s.pathname), p = a.request.cache === "only-if-cached" && !o;
+  t && !n && !p && a.respondWith(
+    (async () => o && await caches.match(a.request) || u(a.request))()
   );
 });
