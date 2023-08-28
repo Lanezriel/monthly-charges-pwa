02a5123c54434cf020789cd803ff86aca9e4be1f
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-28 20:16:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i = [
-  a + "/_app/immutable/entry/app.58be203c.js",
+  a + "/_app/immutable/entry/app.cd1ad179.js",
   a + "/_app/immutable/assets/0.cd0b29c6.css",
-  a + "/_app/immutable/nodes/0.7c6613cd.js",
+  a + "/_app/immutable/nodes/0.91faf630.js",
   a + "/_app/immutable/assets/svelte-logo.87df40b8.svg",
   a + "/_app/immutable/assets/github.1ea8d62e.svg",
   a + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
@@ -11,25 +11,25 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i
   a + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
-  a + "/_app/immutable/nodes/1.4df46f6a.js",
+  a + "/_app/immutable/nodes/1.c9e223fb.js",
   a + "/_app/immutable/assets/2.5d969213.css",
   a + "/_app/immutable/nodes/2.ef4891fe.js",
   a + "/_app/immutable/nodes/3.fe160bd4.js",
   a + "/_app/immutable/chunks/index.afc7ab19.js",
   a + "/_app/immutable/chunks/scheduler.b0c1c2c3.js",
-  a + "/_app/immutable/chunks/singletons.f690e294.js",
-  a + "/_app/immutable/chunks/stores.9781e7a2.js",
-  a + "/_app/immutable/entry/start.49a0a70d.js"
+  a + "/_app/immutable/chunks/singletons.f06fe28b.js",
+  a + "/_app/immutable/chunks/stores.97d54805.js",
+  a + "/_app/immutable/entry/start.4600f95e.js"
 ], m = [
   a + "/.nojekyll",
   a + "/favicon.png",
   a + "/icon192.png",
   a + "/icon512.png",
   a + "/robots.txt"
-], l = "1693246268835", n = self, p = "monthly-charges", c = `${p}_${l}`, r = i.concat(m);
+], l = "1693246606726", n = self, p = "monthly-charges", o = `${p}_${l}`, f = i.concat(m);
 n.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(c).then((s) => s.addAll(r)).then(() => {
+    caches.open(o).then((s) => s.addAll(f)).then(() => {
       n.skipWaiting();
     })
   );
@@ -38,25 +38,25 @@ n.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(async (s) => {
       for (const t of s)
-        t !== c && await caches.delete(t);
+        t !== o && await caches.delete(t);
       n.clients.claim();
     })
   );
 });
-async function f(e) {
-  const s = await caches.open(c);
+async function r(e) {
+  const s = await caches.open(o);
   try {
     const t = await fetch(e);
     return s.put(e, t.clone()), t;
   } catch (t) {
-    const o = await s.match(e);
-    if (o)
-      return o;
+    const c = await s.match(e);
+    if (c)
+      return c;
     throw t;
   }
 }
 n.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((s) => s || f(e.request.clone()))
+    caches.match(e.request).then((s) => s || r(e.request.clone()))
   );
 });
