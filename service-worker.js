90975d5287f3a0a8b007c3dd8e94249dad5491b4
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-28 16:24:11 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  e + "/_app/immutable/entry/app.c66048b3.js",
+const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), p = [
+  e + "/_app/immutable/entry/app.7fb0cf1b.js",
   e + "/_app/immutable/assets/0.cd0b29c6.css",
-  e + "/_app/immutable/nodes/0.9149c860.js",
+  e + "/_app/immutable/nodes/0.fe9ce894.js",
   e + "/_app/immutable/assets/svelte-logo.87df40b8.svg",
   e + "/_app/immutable/assets/github.1ea8d62e.svg",
   e + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
@@ -11,55 +11,55 @@ const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r
   e + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
   e + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
   e + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
-  e + "/_app/immutable/nodes/1.d922718d.js",
+  e + "/_app/immutable/nodes/1.da4cc5b9.js",
   e + "/_app/immutable/assets/2.5d969213.css",
   e + "/_app/immutable/nodes/2.ef4891fe.js",
   e + "/_app/immutable/nodes/3.fe160bd4.js",
   e + "/_app/immutable/chunks/index.afc7ab19.js",
   e + "/_app/immutable/chunks/scheduler.b0c1c2c3.js",
-  e + "/_app/immutable/chunks/singletons.b98020ae.js",
-  e + "/_app/immutable/chunks/stores.1114a870.js",
-  e + "/_app/immutable/entry/start.7882a39f.js"
-], f = [
+  e + "/_app/immutable/chunks/singletons.29f4adf5.js",
+  e + "/_app/immutable/chunks/stores.72f624c3.js",
+  e + "/_app/immutable/entry/start.e2b92626.js"
+], r = [
   e + "/.nojekyll",
   e + "/favicon.png",
   e + "/icon192.png",
   e + "/icon512.png",
   e + "/robots.txt"
-], i = "1693220339906", c = self, l = `cache${i}`, m = r.concat(f), h = new Set(m);
-c.addEventListener("install", (a) => {
-  a.waitUntil(
-    caches.open(l).then((s) => s.addAll(m)).then(() => {
+], f = "1693232646941", c = self, h = "monthly-charges", o = `${h}_${f}`, l = p.concat(r), u = new Set(l);
+c.addEventListener("install", (s) => {
+  s.waitUntil(
+    caches.open(o).then((a) => a.addAll(l)).then(() => {
       c.skipWaiting();
     })
   );
 });
-c.addEventListener("activate", (a) => {
-  a.waitUntil(
-    caches.keys().then(async (s) => {
-      for (const t of s)
-        t !== l && await caches.delete(t);
+c.addEventListener("activate", (s) => {
+  s.waitUntil(
+    caches.keys().then(async (a) => {
+      for (const t of a)
+        t !== o && await caches.delete(t);
       c.clients.claim();
     })
   );
 });
-async function u(a) {
-  const s = await caches.open(`offline${i}`);
+async function b(s) {
+  const a = await caches.open(o);
   try {
-    const t = await fetch(a);
-    return s.put(a, t.clone()), t;
+    const t = await fetch(s);
+    return a.put(s, t.clone()), t;
   } catch (t) {
-    const n = await s.match(a);
+    const n = await a.match(s);
     if (n)
       return n;
     throw t;
   }
 }
-c.addEventListener("fetch", (a) => {
-  if (a.request.method !== "GET" || a.request.headers.has("range"))
+c.addEventListener("fetch", (s) => {
+  if (s.request.method !== "GET" || s.request.headers.has("range"))
     return;
-  const s = new URL(a.request.url), t = s.protocol.startsWith("http"), n = s.hostname === self.location.hostname && s.port !== self.location.port, o = s.host === self.location.host && h.has(s.pathname), p = a.request.cache === "only-if-cached" && !o;
-  t && !n && !p && a.respondWith(
-    (async () => o && await caches.match(a.request) || u(a.request))()
+  const a = new URL(s.request.url), t = a.protocol.startsWith("http"), n = a.hostname === self.location.hostname && a.port !== self.location.port, i = a.host === self.location.host && u.has(a.pathname), m = s.request.cache === "only-if-cached" && !i;
+  t && !n && !m && s.respondWith(
+    (async () => i && await caches.match(s.request) || b(s.request))()
   );
 });
