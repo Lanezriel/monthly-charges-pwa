37969310df236425555dbd5e9dde460337e58893
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-28 17:15:56 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,41 @@
-const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i = [
-  a + "/_app/immutable/entry/app.35eca090.js",
-  a + "/_app/immutable/assets/0.cd0b29c6.css",
-  a + "/_app/immutable/nodes/0.ff634fbd.js",
-  a + "/_app/immutable/assets/svelte-logo.87df40b8.svg",
-  a + "/_app/immutable/assets/github.1ea8d62e.svg",
-  a + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
-  a + "/_app/immutable/assets/fira-mono-all-400-normal.1e3b098c.woff",
-  a + "/_app/immutable/assets/fira-mono-cyrillic-400-normal.c7d433fd.woff2",
-  a + "/_app/immutable/assets/fira-mono-greek-ext-400-normal.9e2fe623.woff2",
-  a + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
-  a + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
-  a + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
-  a + "/_app/immutable/nodes/1.69804bf8.js",
-  a + "/_app/immutable/assets/2.5d969213.css",
-  a + "/_app/immutable/nodes/2.ef4891fe.js",
-  a + "/_app/immutable/nodes/3.fe160bd4.js",
-  a + "/_app/immutable/chunks/index.afc7ab19.js",
-  a + "/_app/immutable/chunks/scheduler.b0c1c2c3.js",
-  a + "/_app/immutable/chunks/singletons.ed94e515.js",
-  a + "/_app/immutable/chunks/stores.61d354fd.js",
-  a + "/_app/immutable/entry/start.4f38a2ba.js"
+const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i = [
+  e + "/_app/immutable/entry/app.43b82c76.js",
+  e + "/_app/immutable/assets/0.cd0b29c6.css",
+  e + "/_app/immutable/nodes/0.5b855211.js",
+  e + "/_app/immutable/assets/svelte-logo.87df40b8.svg",
+  e + "/_app/immutable/assets/github.1ea8d62e.svg",
+  e + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
+  e + "/_app/immutable/assets/fira-mono-all-400-normal.1e3b098c.woff",
+  e + "/_app/immutable/assets/fira-mono-cyrillic-400-normal.c7d433fd.woff2",
+  e + "/_app/immutable/assets/fira-mono-greek-ext-400-normal.9e2fe623.woff2",
+  e + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
+  e + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
+  e + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
+  e + "/_app/immutable/nodes/1.4feacb6f.js",
+  e + "/_app/immutable/assets/2.5d969213.css",
+  e + "/_app/immutable/nodes/2.ef4891fe.js",
+  e + "/_app/immutable/nodes/3.fe160bd4.js",
+  e + "/_app/immutable/chunks/index.afc7ab19.js",
+  e + "/_app/immutable/chunks/scheduler.b0c1c2c3.js",
+  e + "/_app/immutable/chunks/singletons.57d11242.js",
+  e + "/_app/immutable/chunks/stores.9ee5f138.js",
+  e + "/_app/immutable/entry/start.5ee0f5fd.js"
 ], m = [
-  a + "/.nojekyll",
-  a + "/favicon.png",
-  a + "/icon192.png",
-  a + "/icon512.png",
-  a + "/robots.txt"
-], l = "1693235526171", n = self, p = "monthly-charges", o = `${p}_${l}`, f = i.concat(m);
-n.addEventListener("install", (e) => {
-  e.waitUntil(
+  e + "/.nojekyll",
+  e + "/favicon.png",
+  e + "/icon192.png",
+  e + "/icon512.png",
+  e + "/robots.txt"
+], l = "1693235752510", n = self, p = "monthly-charges", o = `${p}_${l}`, f = i.concat(m);
+n.addEventListener("install", (a) => {
+  a.waitUntil(
     caches.open(o).then((s) => s.addAll(f)).then(() => {
       n.skipWaiting();
     })
   );
 });
-n.addEventListener("activate", (e) => {
-  e.waitUntil(
+n.addEventListener("activate", (a) => {
+  a.waitUntil(
     caches.keys().then(async (s) => {
       for (const t of s)
         t !== o && await caches.delete(t);
@@ -43,20 +43,20 @@ n.addEventListener("activate", (e) => {
     })
   );
 });
-async function r(e) {
+async function r(a) {
   const s = await caches.open(o);
   try {
-    const t = await fetch(e);
-    return s.put(e, t.clone()), t;
+    const t = await fetch(a);
+    return s.put(a, t.clone()), t;
   } catch (t) {
-    const c = await s.match(e);
+    const c = await s.match(a);
     if (c)
       return c;
     throw t;
   }
 }
-n.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((s) => s || r(e.request.clone()))
+n.addEventListener("fetch", (a) => {
+  a.respondWith(
+    caches.match(a.request).then((s) => s || r(a.request.clone()))
   );
 });
