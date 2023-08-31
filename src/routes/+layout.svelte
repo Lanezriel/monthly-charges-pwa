1fe885e5a3440b3631e9c204c6da2496cd32f46f
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-31 14:33:40 +0200

    restore iOS changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -98,7 +98,6 @@
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
-    padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
 	}
 
 	main {
