18f4ae07abe03ef7b4280814f3bddb4407f17e3a
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-31 20:06:55 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i = [
-  a + "/_app/immutable/entry/app.a39abab5.js",
-  a + "/_app/immutable/assets/0.12a30921.css",
-  a + "/_app/immutable/nodes/0.99779e4f.js",
+  a + "/_app/immutable/entry/app.2dd249f2.js",
+  a + "/_app/immutable/assets/0.fe47eb9b.css",
+  a + "/_app/immutable/nodes/0.8f33acf1.js",
   a + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
   a + "/_app/immutable/assets/fira-mono-all-400-normal.1e3b098c.woff",
   a + "/_app/immutable/assets/fira-mono-cyrillic-400-normal.c7d433fd.woff2",
@@ -9,16 +9,18 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i
   a + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
-  a + "/_app/immutable/nodes/1.a7d3ebe4.js",
+  a + "/_app/immutable/nodes/1.cec6c6d9.js",
   a + "/_app/immutable/assets/2.9d45a74d.css",
-  a + "/_app/immutable/nodes/2.cb6b8345.js",
+  a + "/_app/immutable/nodes/2.e9eb04f9.js",
   a + "/_app/immutable/assets/3.203a94cf.css",
-  a + "/_app/immutable/nodes/3.95a67d57.js",
+  a + "/_app/immutable/nodes/3.416eecdb.js",
+  a + "/_app/immutable/nodes/4.28489c1d.js",
   a + "/_app/immutable/chunks/index.43038c9e.js",
+  a + "/_app/immutable/chunks/paths.7a8c03ca.js",
   a + "/_app/immutable/chunks/scheduler.a00299e9.js",
-  a + "/_app/immutable/chunks/singletons.5c230825.js",
-  a + "/_app/immutable/chunks/stores.d0ae1399.js",
-  a + "/_app/immutable/entry/start.445c918d.js"
+  a + "/_app/immutable/chunks/singletons.c1a4b76c.js",
+  a + "/_app/immutable/chunks/stores.ece2d823.js",
+  a + "/_app/immutable/entry/start.11946036.js"
 ], m = [
   a + "/.nojekyll",
   a + "/favicon.png",
@@ -26,10 +28,10 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i
   a + "/icon512.png",
   a + "/robots.txt",
   a + "/touch-icon.png"
-], l = "1693493222333", n = self, p = "monthly-charges", o = `${p}_${l}`, r = i.concat(m);
+], p = "1693505210064", n = self, l = "monthly-charges", c = `${l}_${p}`, r = i.concat(m);
 n.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(o).then((s) => s.addAll(r)).then(() => {
+    caches.open(c).then((s) => s.addAll(r)).then(() => {
       n.skipWaiting();
     })
   );
@@ -38,20 +40,20 @@ n.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(async (s) => {
       for (const t of s)
-        t !== o && await caches.delete(t);
+        t !== c && await caches.delete(t);
       n.clients.claim();
     })
   );
 });
 async function f(e) {
-  const s = await caches.open(o);
+  const s = await caches.open(c);
   try {
     const t = await fetch(e);
     return s.put(e, t.clone()), t;
   } catch (t) {
-    const c = await s.match(e);
-    if (c)
-      return c;
+    const o = await s.match(e);
+    if (o)
+      return o;
     throw t;
   }
 }
