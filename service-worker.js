f388bee5666f7834dcdc8427d79181429c2846a7
Lanezriel <dimitri.delbrouck@gmail.com>
2023-08-31 13:45:58 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i = [
-  a + "/_app/immutable/entry/app.d965ac9a.js",
-  a + "/_app/immutable/assets/0.c98876d0.css",
-  a + "/_app/immutable/nodes/0.068affe7.js",
+  a + "/_app/immutable/entry/app.cd83ffc5.js",
+  a + "/_app/immutable/assets/0.12a30921.css",
+  a + "/_app/immutable/nodes/0.d641b410.js",
   a + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
   a + "/_app/immutable/assets/fira-mono-all-400-normal.1e3b098c.woff",
   a + "/_app/immutable/assets/fira-mono-cyrillic-400-normal.c7d433fd.woff2",
@@ -9,16 +9,16 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i
   a + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
-  a + "/_app/immutable/nodes/1.b8207584.js",
+  a + "/_app/immutable/nodes/1.4034b7fe.js",
   a + "/_app/immutable/assets/2.e64809d5.css",
   a + "/_app/immutable/nodes/2.bf29c88e.js",
   a + "/_app/immutable/assets/3.203a94cf.css",
   a + "/_app/immutable/nodes/3.ab1b314c.js",
   a + "/_app/immutable/chunks/index.43038c9e.js",
   a + "/_app/immutable/chunks/scheduler.a00299e9.js",
-  a + "/_app/immutable/chunks/singletons.40dc8ab8.js",
-  a + "/_app/immutable/chunks/stores.ff8ac355.js",
-  a + "/_app/immutable/entry/start.a564182b.js"
+  a + "/_app/immutable/chunks/singletons.e40ba707.js",
+  a + "/_app/immutable/chunks/stores.d57808bd.js",
+  a + "/_app/immutable/entry/start.a1f724b1.js"
 ], m = [
   a + "/.nojekyll",
   a + "/favicon.png",
@@ -26,7 +26,7 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), i
   a + "/icon512.png",
   a + "/robots.txt",
   a + "/touch-icon.png"
-], l = "1693481037552", n = self, p = "monthly-charges", c = `${p}_${l}`, r = i.concat(m);
+], l = "1693482354607", n = self, p = "monthly-charges", c = `${p}_${l}`, r = i.concat(m);
 n.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(c).then((s) => s.addAll(r)).then(() => {
