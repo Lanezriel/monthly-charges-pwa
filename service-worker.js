e75f675084f75918382648ab8203e1e0f139be43
Lanezriel <dimitri.delbrouck@gmail.com>
2023-09-05 15:43:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), o = [
-  a + "/_app/immutable/entry/app.ebfdba3f.js",
-  a + "/_app/immutable/assets/0.55fe2f70.css",
-  a + "/_app/immutable/nodes/0.5e6ab924.js",
+  a + "/_app/immutable/entry/app.db602583.js",
+  a + "/_app/immutable/assets/0.affc57d6.css",
+  a + "/_app/immutable/nodes/0.5b77f634.js",
   a + "/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2",
   a + "/_app/immutable/assets/fira-mono-all-400-normal.1e3b098c.woff",
   a + "/_app/immutable/assets/fira-mono-cyrillic-400-normal.c7d433fd.woff2",
@@ -9,21 +9,22 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), o
   a + "/_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2",
   a + "/_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2",
-  a + "/_app/immutable/nodes/1.bb79ea34.js",
-  a + "/_app/immutable/assets/2.7183e2f2.css",
-  a + "/_app/immutable/nodes/2.dd82ea34.js",
-  a + "/_app/immutable/assets/3.0012764f.css",
-  a + "/_app/immutable/nodes/3.c59f0cdf.js",
-  a + "/_app/immutable/assets/4.378cc837.css",
-  a + "/_app/immutable/nodes/4.99649747.js",
+  a + "/_app/immutable/nodes/1.20859e95.js",
+  a + "/_app/immutable/assets/2.6cec3a9a.css",
+  a + "/_app/immutable/nodes/2.7426892b.js",
+  a + "/_app/immutable/assets/3.ed28a95f.css",
+  a + "/_app/immutable/nodes/3.aae22eb0.js",
+  a + "/_app/immutable/assets/4.f9eda6c5.css",
+  a + "/_app/immutable/nodes/4.b927ae1d.js",
   a + "/_app/immutable/chunks/createRipple.6d227583.js",
+  a + "/_app/immutable/chunks/index.4b1ac4b1.js",
   a + "/_app/immutable/chunks/index.7e7356c5.js",
-  a + "/_app/immutable/chunks/index.a6901bfe.js",
-  a + "/_app/immutable/chunks/paths.effd525d.js",
+  a + "/_app/immutable/chunks/index.bd3e99d2.js",
+  a + "/_app/immutable/chunks/paths.6ae6b44c.js",
   a + "/_app/immutable/chunks/scheduler.e051e6e5.js",
-  a + "/_app/immutable/chunks/singletons.6c49f657.js",
-  a + "/_app/immutable/chunks/stores.14182921.js",
-  a + "/_app/immutable/entry/start.b18d3a0c.js"
+  a + "/_app/immutable/chunks/singletons.506cdd9e.js",
+  a + "/_app/immutable/chunks/stores.f8f6312a.js",
+  a + "/_app/immutable/entry/start.0b54996b.js"
 ], m = [
   a + "/.nojekyll",
   a + "/favicon.png",
@@ -31,10 +32,10 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), o
   a + "/icon512.png",
   a + "/robots.txt",
   a + "/touch-icon.png"
-], p = "1693914813600", n = self, l = "monthly-charges", c = `${l}_${p}`, f = o.concat(m);
+], p = "1693921410468", n = self, l = "monthly-charges", c = `${l}_${p}`, r = o.concat(m);
 n.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(c).then((s) => s.addAll(f)).then(() => {
+    caches.open(c).then((s) => s.addAll(r)).then(() => {
       n.skipWaiting();
     })
   );
@@ -48,7 +49,7 @@ n.addEventListener("activate", (e) => {
     })
   );
 });
-async function r(e) {
+async function b(e) {
   const s = await caches.open(c);
   try {
     const t = await fetch(e);
@@ -62,6 +63,6 @@ async function r(e) {
 }
 n.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((s) => s || r(e.request.clone()))
+    caches.match(e.request).then((s) => s || b(e.request.clone()))
   );
 });
