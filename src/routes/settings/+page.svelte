c0be124ec896fa6af5d7f00db1c546eef897d832
Lanezriel <dimitri.delbrouck@gmail.com>
2023-09-06 09:01:53 +0200

    Add select for Currency

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -9,10 +9,18 @@
 
   const settings = getContext('settings');
 
+  let currencySelect;
+
   function navigate(event, path) {
     createRipple(event);
 
     goto(path);
+  }
+
+  function onCurrencyClick(event) {
+    createRipple(event);
+
+    currencySelect.click();
   }
 </script>
 
@@ -28,25 +36,60 @@
 </ItemWrapper>
 
 <ItemWrapper>
-  <ItemEntry clickCallback={(e) => createRipple(e)}>
-    <h2>Currency</h2>
-    <p>{$settings.currency}</p>
+  <ItemEntry noPadding clickCallback={onCurrencyClick}>
+    <div class="select-wrapper">
+      <label for="currency">Currency</label>
+      <select
+        id="currency"
+        bind:value={$settings.currency}
+        bind:this={currencySelect}
+      >
+        <option value="€">€</option>
+        <option value="$">$</option>
+        <option value="¥">¥</option>
+      </select>
+    </div>
   </ItemEntry>
 </ItemWrapper>
 
 <style>
-  h2 {
+  h2, label {
     font-size: 1.2rem;
     font-weight: bold;
     margin: 0;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
+    pointer-events: none;
   }
 
-  p {
-    flex-shrink: 0;
+  .select-wrapper {
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+
+  .select-wrapper > label {
+    position: absolute;
+    left: 1rem;
+    top: 50%;
+    transform: translateY(-50%);
+    max-width: 75%;
+  }
+
+  select {
+    width: 100%;
+    height: 100%;
     margin: 0;
+    padding: 0 1rem;
+    text-align: right;
     font-size: 1.2rem;
+    background: none;
+    color: var(--color-text);
+    border: none;
+    appearance: none;
+    outline: none;
+    cursor: pointer;
+    z-index: 2;
   }
 </style>
